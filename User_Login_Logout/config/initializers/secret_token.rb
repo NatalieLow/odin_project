0a3cc052d0a29f50053f59e8d52e28c15985cba0
Natalie Low <gnatann@gmail.com>
2014-11-05 09:22:09 -0800

    Added Login_Logout

---
+++ User_Login_Logout/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+UserModelTutorial::Application.config.secret_key_base = '07dc7a0309d1de3846ac987813a2a8d27f37d4b94289640fc8333e59821b668f4712f9a2f4cdda5ccebf320079b3fd5d9972a040269337e43030f496b3088782'
